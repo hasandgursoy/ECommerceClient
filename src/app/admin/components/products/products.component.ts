43335de21fb5c18fdf03b8b445759adcb7630ea4
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-05-16 23:39:22 +0300

    EndPoint Settings Started

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
   selector: 'app-products',
@@ -9,12 +11,15 @@ import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 })
 export class ProductsComponent extends BaseComponent implements OnInit {
 
-  constructor(spinner:NgxSpinnerService) {
+  constructor(spinner:NgxSpinnerService,private httpClientService : HttpClientService) {
     super(spinner);
   }
 
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallAtom)
+    this.httpClientService.get({
+      controller:"products"
+    }).subscribe(data => console.log(data))
   }
 
 }
---
+++ src/app/services/common/http-client.service.ts
@@ -0,0 +1,53 @@
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http"
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpClientService {
+
+  constructor(private httpClient: HttpClient, @Inject("baseUrl") private baseUrl: string) { }
+
+  private url(requestParameters: Partial<RequestParameters>): string {
+    return `${requestParameters.baseUrl ? requestParameters.baseUrl : this.baseUrl}/${requestParameters.controller}${requestParameters.action ? `/${requestParameters.action}` : ""}`;
+  };
+
+  get<T>(requestParameters: Partial<RequestParameters>,id?:string)  :Observable<T>{
+
+    
+
+    let url: string = "";
+    if (requestParameters.fullEndPoint) {
+      url = requestParameters.fullEndPoint;
+    } else {
+      url = `${this.url(requestParameters)}${id ? `/${id}`:""}`;
+
+    }
+
+    return this.httpClient.get<T>(url,{headers:requestParameters.headers});
+
+  }
+  
+  postMessage() {
+
+  }
+
+  put() {
+
+  }
+
+  delete() {
+
+  }
+
+
+}
+
+export class RequestParameters {
+  controller?: string;
+  action?: string;
+  headers?: HttpHeaders;
+  baseUrl?: string;
+  fullEndPoint?: string
+}
